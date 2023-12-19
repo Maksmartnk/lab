e62ea44f93b66c029d8150c3ed880131055b5629
Maksmartnk <153653160+Maksmartnk@users.noreply.github.com>
2023-12-19 15:52:02 +0200

    Update beta_work_py.py

--- beta_work_py.py
+++ beta_work_py.py
@@ -3,26 +3,16 @@ while el != "end":
     try:
         el = int(input("1 , 2 : " ))
         if el == 1 :
-            input_text = input("Введіть текст: ")
+            w = input("write text:")
+            list_nums = list(w)
+            set_text = set(list_nums)
+            sort_text = sorted(set_text) 
+            print(list_nums)
+            print("кількість елементів", len(list_nums))
+            for el in sort_text:
+                print(el, "=" , sort_text.count(el))
 
-        # створюю пустий словник який буду заповнювати
-            letter_count = {}
 
-        # Перебераю кожен ключ
-            for char in input_text:
-                
-        # пробускаю пробіли
-                if char != ' ':
-                
-                    letter_count[char] = letter_count.get(char, 0) + 1
-
-    # результат
-            print("Кількість кожної літери у введеному тексті:")
-            for letter, count in letter_count.items():
-                print(f"{letter}: {count}")
-
-        if el == 2 :
-            
             
     except ValueError:
         print("помилка значення")
